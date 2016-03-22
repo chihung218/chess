90d1809a0d6f3fcff11f35acf41ca419b1c97edf
chihung218 <chi.hung.wong.218@gmail.com>
2016-03-22 17:15:50 +0800

    chess

---
+++ chess.py
@@ -0,0 +1,81 @@
+class Player:
+    def __init__(self,name):
+        self.name=name
+
+    def printName(self):
+        print(self.name)
+
+    def get(self):
+        return self.name
+
+class Chess:
+    pieces={"Rat","Cat","Dog","Wolf","Leopard","Tiger","Lion","Elephant"}
+    def __init__(self,rank,color):
+        self.rank=rank
+        self.color=color
+        self.piece=pieces[sel.rank]
+        self.jump=False
+        self.swim=False
+        
+class Board:
+    def __init__(self):
+        self.chessBoard=[[0]*7]*9       
+    def start(self):
+        for i in range(0,9):
+            for j in range(0,7):
+                self.chessBoard[i][j]=0               
+    def printBoard(self):
+        print("--|",sep="",end="")
+        for x in range(0,7):
+            print("|%3s"%chr(65+x),sep="",end="")
+        print("|\n--|+---+---+---+---+---+---+---+")
+        for i in range(0,9):
+            print("%2d|"%i,sep="",end="")
+            for j in range(0,7):
+                print("|%3s"%self.chessBoard[i][j],sep="",end="")
+            print("|\n--|+---+---+---+---+---+---+---+")
+    def updatePosition(self,chess,x,y):
+        self.chessBoard[x][y]=chess
+        printBoard()
+class Game:
+    def __init__(self):
+        self.running=False
+        self.playerA=None
+        self.playerB=None
+        self.winner=None
+        self.board=None
+    def gameStart(self):
+        self.running=True
+        self.playerA=Player("A")
+        self.playerB=Player("B")
+        self.board=Board()
+        self.board.start()
+        self.board.printBoard()
+    def getPlayer(self,player):
+        if player.upper=="A":
+            return self.playerA
+        elif player.upper=="B":
+            return self.playerB
+        else:
+            #print("None such player")
+            return None
+    def endGame(self,winner=None):
+        #print(winner)
+        if (winner is None):
+            raise TypeError("Invalid Input")
+        #elif(self.running & ((self.getPlayer(winner)==self.playerA)|(self.getPlayer(winner)==self.playerB))):
+#            self.running=False
+#            self.winner=winner
+        #else:
+#            print("Game Cannot End")
+
+    #def playerTurn(self):
+        
+def main():
+    g=Game()
+    g.gameStart()
+    g.endGame()
+
+    
+if __name__=="__main__":
+    main()
